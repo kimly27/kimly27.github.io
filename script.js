cacfd3ba887a863f663288920e5fa482c51cf74e
Kimberly Mae Kho <kimlykho27@gmail.com>
2025-07-02 11:19:06 +0800

    complete sections

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//for roles animation
 const roles = [
     "QA Tester",
     "UI/UX Designer",
@@ -19,3 +20,45 @@ setInterval(() => {
         roleEl.classList.add("fade-in");
     }, 500);
 }, 3000);
+
+
+
+//for project modal
+function openProjectModal(title, company, date, imageSrc) {
+    document.getElementById("modalTitle").innerText = title;
+    document.getElementById("modalCompany").innerText = company;
+    document.getElementById("modalDate").innerText = date;
+    document.getElementById("modalImage").src = imageSrc;
+    document.getElementById("projectModal").classList.remove("hidden");
+}
+
+function closeModal() {
+    document.getElementById("projectModal").classList.add("hidden");
+}
+
+//click outside modal to close
+window.addEventListener("click", function (e) {
+    const modal = document.getElementById("projectModal");
+    if (e.target === modal) {
+        closeModal();
+    }
+});
+
+document.querySelectorAll(".project-card").forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const title = btn.getAttribute("data-title");
+        const company = btn.getAttribute("data-company");
+        const date = btn.getAttribute("data-date");
+        const image = btn.getAttribute("data-image");
+
+        openProjectModal(title, company, date, image);
+    });
+});
+
+//mobile menu toggle
+const toggleBtn = document.getElementById('mobile-menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+toggleBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
